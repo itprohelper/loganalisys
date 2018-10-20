be6ef01bb213c1068d10b4ba2f90f080ee1c5b99
rudy@itprohelper.com <rudy@itprohelper.com>
2018-10-20 02:49:40 +0000

    corrections according to PEP8 style recommendations

--- logAnalysis.py
+++ logAnalysis.py
@@ -5,59 +5,54 @@ conn = psycopg2.connect("dbname=news")
 
 cursor = conn.cursor()
 
-cursor.execute ("""
-SELECT title, count(*) AS views 
-FROM articles JOIN log 
-ON log.path = '/article/' || articles.slug 
-GROUP BY articles.title 
+cursor.execute("""
+SELECT title, count(*) AS views
+FROM articles JOIN log
+ON log.path = '/article/' || articles.slug
+GROUP BY articles.title
 ORDER BY views DESC LIMIT 3;
 """)
 
 results = cursor.fetchall()
 
-#print results
+# print results
 
 print
 print "Most popular three articles of all time"
-#for results in results:
-#  print " ", results[0], results[1],("-- views")
 for title, views in results:
     print('"{}" - {} views'.format(title, views))
 
-
 print
-cursor.execute ("""
-SELECT name, count(*) AS views 
-FROM authors, articles JOIN log 
+cursor.execute("""
+SELECT name, count(*) AS views
+FROM authors, articles JOIN log
 ON log.path = '/article/' || articles.slug
-WHERE authors.id = articles.author 
-GROUP BY authors.name 
+WHERE authors.id = articles.author
+GROUP BY authors.name
 ORDER BY views DESC LIMIT 3;
 """)
 
 results = cursor.fetchall()
 
-
 print
 print "Most popular article authors of all time"
-for results in results:
-  print " ", results[0], results[1],("-- views")
+for name, views in results:
+    print('"{}" - {} views'.format(name, views))
 
 print
-cursor.execute ("""
-SELECT date, avg(num) 
-FROM errorsperday 
-WHERE num > 1.0 
-GROUP BY date 
+cursor.execute("""
+SELECT date, avg(num)
+FROM errorsperday
+WHERE num > 1.0
+GROUP BY date
 ORDER BY date DESC;
 """)
 
 results = cursor.fetchall()
 
-
 print
 print "Days did more than 1% of requests lead to errors"
 for results in results:
-  print " ", results[0], "{0:.0%}".format(results[1]/100)
+    print " ", results[0], "{0:.0%}".format(results[1]/100)
 
 conn.close()
